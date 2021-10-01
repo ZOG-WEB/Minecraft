e8f1efd2d8303cbf6a70d89de67f54f79cc10405
ZOG-WEB <79850764+ZOG-WEB@users.noreply.github.com>
2021-10-01 18:27:08 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -147,3 +147,46 @@ function down(){
                 player_update();
             }
             }
+    function up(){
+ if(player_y>=0){
+     player_y=player_y-block_image_height;
+     console.log("block image height=" + block_image_height);
+     console.log("when up arrow key is pressed, x = " + player_x + "y= " + player_y);
+     canvas.remove(player_object);
+player_update();
+ }
+ }
+
+ function down(){
+    if(player_y<=600){
+        player_y=player_y+block_image_height;
+        console.log("block image height=" + block_image_height);
+        console.log("when down arrow key is pressed, x = " + player_x + "y= " + player_y);
+        canvas.remove(player_object);
+   player_update();
+    }
+    }
+
+    function left(){
+        if(player_x>=0){
+            player_x=player_x-block_image_width;
+            console.log("block image width=" + block_image_width);
+            console.log("when left arrow key is pressed, x = " + player_x + "y= " + player_y);
+            canvas.remove(player_object);
+       player_update();
+        }
+        }
+
+        function right(){
+            if(player_x<=1000){
+                player_x=player_x+block_image_width;
+                console.log("block image width=" + block_image_width);
+                console.log("when right arrow key is pressed, x = " + player_x + "y= " + player_y);
+                canvas.remove(player_object);
+           player_update();
+            }
+            }
+
+
+
+
